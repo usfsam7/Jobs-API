8e195ad2acd4c20a4bbcb8198ab2986942384241
Youssef sameh <yousameh2006@gmail.com>
2023-01-10 05:10:11 +0200

    <>

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
      const express = require('express')
       const router = express.Router()
       const authController = require('../Controllers/authController')
-      
+      const authMiddleware = require('../Middleware/authMiddleware')
 
 
           // Create User => (Registration) ..
