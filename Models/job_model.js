40ac50244b30f04db04465d8382e209b63902b45
Youssef sameh <yousameh2006@gmail.com>
2023-06-01 10:07:58 +0300

    <>

--- Models/job_model.js
+++ Models/job_model.js
@@ -1,37 +1,45 @@
 const mongoose = require('mongoose')
 
 
- const jobSchema = new mongoose.Schema({
-   fullName: {
-    required: true,
-     type: String,
-     minLength: [5, "FullName Length Must be at least 5"],
-     maxLength: [45, "FullName Length Must be 45 Or Less"],
+ const jobSchema = new mongoose.Schema(
+   {
+     fullName: {
+       required: true,
+       type: String,
+       minLength: [5, "FullName Length Must be at least 5"],
+       maxLength: [45, "FullName Length Must be 45 Or Less"],
+     },
+     email: {
+       required: true,
+       type: String,
+       match: [
+         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+         "Provide a valid email",
+       ],
+     },
+     address: {
+       required: true,
+       type: String,
+       maxLength: [45, "Address Length Must be 45 Or Less"],
+     },
+     dateOfBirth: {
+       required: true,
+       type: String,
+       match: [
+         /^\d{4}-\d{2}-\d{2}$/,
+         "Invalid Date Format, it must be Like year-month-days (yyyy-mm-dd)",
+       ],
+     },
+     userImagePath: {
+       type: String,
+     },
+     createdAt: {
+       type: Schema.Types.Timestamp,
+       default: Date.now 
+    }
    },
-   email: {
-    required: true,
-     type: String,
-     match: [
-       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-       "Provide a valid email",
-     ],
-   },
-   address: {
-    required: true,
-     type: String,
-     maxLength: [45, "Address Length Must be 45 Or Less"],
-   },
-   dateOfBirth: {
-    required: true,
-     type: String,
-     match: [/^\d{4}-\d{2}-\d{2}$/, 
-      'Invalid Date Format, it must be Like year-month-days (yyyy-mm-dd)'
-    ],
-   },
-   userImagePath: {
-     type: String,
-   },
- }, { versionKey: false });
+   { versionKey: false }
+ );
 
 
   const Job = mongoose.model('Job', jobSchema);
