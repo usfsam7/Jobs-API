282a665047993a026cf107fc1d58fd37ee802354
Youssef sameh <yousameh2006@gmail.com>
2023-06-02 06:42:28 +0300

    <>

--- Models/job_model.js
+++ Models/job_model.js
@@ -14,7 +14,7 @@ const mongoose = require('mongoose')
        type: String,
        match: [
          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-         "Provide a valid email",
+         "Provide a correct email",
        ],
      },
      address: {
